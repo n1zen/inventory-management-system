08ab19afcac9889c622ff4c7f21339926bd498b4
n1zen <114575802+n1zen@users.noreply.github.com>
2025-09-07 00:52:12 +0800

    Add forms

--- client/src/pages/CreateProductPage.jsx
+++ client/src/pages/CreateProductPage.jsx
@@ -8,6 +8,9 @@ import toast from 'react-hot-toast';
 //import axios instance
 import api from '../lib/axios.js';
 
+// import css
+import '../styles/pages/CreateProductPage.css'
+
 const CreateProductPage = () => {
 
   // product info states
@@ -35,6 +38,50 @@ const CreateProductPage = () => {
           <ArrowLeftIcon className="link-icon"/>
           <span className="link-text">Back to Home</span>
         </Link>
+      </div>
+      <div className="card-form-container">
+        <h2 className="form-title">Create New Product</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Product Name</span>
+            </label>
+            <input type="text" 
+                placeholder="Product Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <label className="label">
+              <span className="label-text">SKU</span>
+            </label>
+            <input type="number" 
+                value={sku}
+                onChange={(e) => setSku(e.target.value)}
+            />
+            <label className="label">
+              <span className="label-text">Price</span>
+            </label>
+            <input type="number"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+            />
+            <label className="label">
+              <span className="label-text">Series</span>
+            </label>
+            <input type="text"
+                placeholder="Category"
+                value={series}
+                onChange={(e) => setSeries(e.target.value)}
+            />
+            <label className="label">
+              <span className="label-text">Stock</span>
+            </label>
+            <input type="number"
+                value={stock}
+                onChange={(e) => setStock(e.target.value)}
+            />
+          </div>
+        </form>
       </div>
     </div>
   )
